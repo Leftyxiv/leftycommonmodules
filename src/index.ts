b84eac960c317530b2427a5e224faa2ad4b407a9
Leftyxiv <manny@aledoux.net>
2021-04-27 18:16:31 -0400

    added some config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 14,
+  blue: 14,
+  green: 14
+}
+
+console.log(color)
+
+export default color;
