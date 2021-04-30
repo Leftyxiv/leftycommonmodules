87d5329d69eddd9a5c3be8850fafec3c4ba1fe50
Leftyxiv <manny@aledoux.net>
2021-04-30 12:00:26 -0400

    Updates

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,9 @@ export * from './middlewares/currentUser';
 export * from './middlewares/errorHandler';
 export * from './middlewares/validateRequest';
 export * from './middlewares/requireAuth';
+// ------------------ EVENTS -----------------------
+export * from './events/baseListener'
+export * from './events/basePublisher'
+export * from './events/subjects'
+export * from './events/TicketCreatedEvent'
+export * from './events/TicketUpdatedEvent';
